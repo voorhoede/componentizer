a06ef43ccdfd2b80cd44421dc2712b9aa29aedcb
Sjoerd Beentjes <mail@sjoerdbeentjes.nl>
2019-01-05 13:55:40 +0100

    refactor: simplify createCloudinary

--- src/lib/createCloudinary.ts
+++ src/lib/createCloudinary.ts
@@ -1,49 +1,44 @@
-function createCloudinary (cloudName: string, unsignedUploadPreset: string, onSuccess: Function): object {
-  return {
-    process: (fieldName: string, file: File, metadata: object, load: Function, error: Function, progress: Function, abort: Function) => {
+const createCloudinary = (cloudName: string, unsignedUploadPreset: string, onSuccess: Function): object => ({
+  process: (fieldName: string, file: File, metadata: object, load: Function, error: Function, progress: Function, abort: Function) => {
+    // `fieldName` and `meta` are not used for now  
+    const url = `https://api.cloudinary.com/v1_1/${cloudName}/upload`;
+    const xhr = new XMLHttpRequest();
+    const formData = new FormData();
 
-      // `fieldName` and `meta` are not used for now
+    xhr.open('POST', url, true);
+    xhr.setRequestHeader('X-Requested-With', 'XMLHttpRequest');
 
-      const url = `https://api.cloudinary.com/v1_1/${cloudName}/upload`;
-      const xhr = new XMLHttpRequest();
-      const formData = new FormData();
+    xhr.upload.addEventListener('progress', e => {
+      progress(e.lengthComputable, e.loaded, e.total);
+    });
 
-      xhr.open('POST', url, true);
-      xhr.setRequestHeader('X-Requested-With', 'XMLHttpRequest');
-
-      xhr.upload.addEventListener('progress', e => {
-        progress(e.lengthComputable, e.loaded, e.total);
-      });
-
-      xhr.onreadystatechange = e => {
-
-        if (xhr.readyState !== 4) {
-          return;
-        }
-
-        if (xhr.status >= 200 && xhr.status < 300) {
-          const response = JSON.parse(xhr.responseText);
-          onSuccess(response);
-          load(response.secure_url);
-          return;
-        }
-
-        error('oh no!');
-      };
-
-      formData.append('upload_preset', unsignedUploadPreset);
-      formData.append('tags', 'browser_upload');
-      formData.append('file', file);
-      xhr.send(formData);
-
-      return {
-        abort: () => {
-          xhr.abort();
-        }
+    xhr.onreadystatechange = e => {
+      if (xhr.readyState !== 4) {
+        return;
       }
-    },
-    revert: null
-  }
-}
+
+      if (xhr.status >= 200 && xhr.status < 300) {
+        const response = JSON.parse(xhr.responseText);
+        onSuccess(response);
+        load();
+        return;
+      }
+
+      error('oh no!');
+    };
+
+    formData.append('upload_preset', unsignedUploadPreset);
+    formData.append('tags', 'browser_upload');
+    formData.append('file', file);
+    xhr.send(formData);
+    
+    return {
+      abort: () => {
+        xhr.abort();
+      }
+    }
+  },
+  revert: null
+});
 
 export default createCloudinary
