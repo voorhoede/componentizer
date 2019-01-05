10b012d1686ebfaa1e4faeec1c6f43bc9f4c5d2f
Sjoerd Beentjes <mail@sjoerdbeentjes.nl>
2019-01-05 22:22:30 +0100

    refactor(AppRouter): remove comments

--- src/AppRouter.tsx
+++ src/AppRouter.tsx
@@ -3,18 +3,13 @@ import { BrowserRouter as Router, Route } from "react-router-dom";
 import Index from './pages/Index';
 import Edit from './pages/Edit'
 
-// const Index = React.lazy(() => import('./pages/Index'))
-// const Edit = React.lazy(() => import('./pages/Edit'))
-
 const AppRouter = () => (
-  // <React.Suspense fallback={<p>Loading...</p>} >
-    <Router>
-      <React.Fragment>
-        <Route path="/" exact component={() => <Index />} />
-        <Route path="/edit/" component={() => <Edit />} />
-      </React.Fragment>
-    </Router>
-  // </React.Suspense>
+  <Router>
+    <React.Fragment>
+      <Route path="/" exact component={() => <Index />} />
+      <Route path="/edit/" component={() => <Edit />} />
+    </React.Fragment>
+  </Router>
 );
 
 export default AppRouter;
