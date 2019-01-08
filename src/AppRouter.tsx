e7c3702800ed2444ac03fe5d4df2bb7fe15e5d3d
Sjoerd Beentjes <mail@sjoerdbeentjes.nl>
2019-01-08 13:27:20 +0100

    feat(AppRouter): use bundle splitting for pages

--- src/AppRouter.tsx
+++ src/AppRouter.tsx
@@ -1,15 +1,18 @@
 import * as React from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
-import Index from './pages/Index';
-import Edit from './pages/Edit'
+
+const Index = React.lazy(() => import('./pages/Index'))
+const Edit = React.lazy(() => import('./pages/Edit'))
 
 const AppRouter = () => (
-  <Router>
-    <React.Fragment>
-      <Route path="/" exact component={() => <Index />} />
-      <Route path="/edit/" component={() => <Edit />} />
-    </React.Fragment>
-  </Router>
+  <React.Suspense fallback={null}>
+    <Router>
+      <React.Fragment>
+        <Route path="/" exact component={() => <Index />} />
+        <Route path="/edit/" component={() => <Edit />} />
+      </React.Fragment>
+    </Router>
+  </React.Suspense>
 );
 
 export default AppRouter;
