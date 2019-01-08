2e1c26837e918145879eeb08f114cf6bf7ad099c
Sjoerd Beentjes <mail@sjoerdbeentjes.nl>
2019-01-08 22:37:04 +0100

    init trello proxy

--- src/lambda/trello-proxy.js
+++ src/lambda/trello-proxy.js
@@ -1,6 +1,31 @@
+import got from 'got'
+import queryString from 'query-string'
+
+const key = process.env.REACT_APP_TRELLO_KEY;
+const baseUrl = 'https://trello.com/1';
+
 export const handler = function(event, context, callback) {
-  callback(null, {
-    statusCode: 200,
-    body: "Hello world"
+  const path = event.path.replace('/trello-proxy', '')
+  const query = queryString.stringify(event.queryStringParameters)
+  const url = `${path}?${query}`
+
+  got({
+    method: event.httpMethod,
+    url: `${baseUrl}${url}`
   })
+    .then(res => {
+      console.log(typeof res.body)
+      callback(null, {
+        statusCode: 200,
+        data: res.body,
+        headers: { 'Content-Type': 'application/json' },
+      })
+    })
+    .catch(err => {
+      callback(null, {
+        statusCode: 400,
+        data: err,
+        headers: { 'Content-Type': 'application/json' },
+      })
+    })
 }
